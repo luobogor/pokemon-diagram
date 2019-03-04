9f46b4c21ff8a03fbbef1e98bf4ed6178382ada2
jinzhanye <jinzhan.ye@foxmail.com>
2019-03-04 21:36:31 +0800

    完成拖拽元素

--- src/graph/Graph.js
+++ src/graph/Graph.js
@@ -1,8 +1,97 @@
 import mxgraph from './index';
 
-const { mxGraph } = mxgraph;
+const {
+  mxGraph,
+  mxVertexHandler,
+  mxConstants,
+  mxCellState,
+  mxPerimeter,
+} = mxgraph;
 
 class Graph extends mxGraph {
+  constructor(container) {
+    super(container);
+    this._init();
+  }
+
+  _init() {
+    this._setDefaultStyle();
+  }
+
+  _setDefaultStyle() {
+    // TODO
+    this.setHtmlLabels(true);
+    this._setDefaultVertexStyle();
+    this._setDefaultEdgeStyle();
+  }
+
+  _setDefaultVertexStyle() {
+    this.foldingEnabled = false;
+
+    const normalTypeStyle = {
+      [mxConstants.STYLE_SHAPE]: mxConstants.SHAPE_IMAGE,
+      [mxConstants.STYLE_PERIMETER]: mxPerimeter.RectanglePerimeter,
+    };
+    this.getStylesheet().putCellStyle('normalType', normalTypeStyle);
+
+    const nodeStyle = {
+      // TODO 标注在哪个 example 可以找到
+      [mxConstants.STYLE_SHAPE]: mxConstants.SHAPE_LABEL,
+      [mxConstants.STYLE_PERIMETER]: mxPerimeter.RectanglePerimeter,
+      [mxConstants.STYLE_ROUNDED]: true,
+      [mxConstants.STYLE_ARCSIZE]: 6, // 设置圆角程度
+
+      [mxConstants.STYLE_STROKECOLOR]: '#333333',
+      [mxConstants.STYLE_FONTCOLOR]: '#333333',
+      [mxConstants.STYLE_FILLCOLOR]: '#ffffff',
+      //
+      [mxConstants.STYLE_LABEL_BACKGROUNDCOLOR]: 'none',
+
+      [mxConstants.STYLE_ALIGN]: mxConstants.ALIGN_CENTER,
+      [mxConstants.STYLE_VERTICAL_ALIGN]: mxConstants.ALIGN_TOP,
+      [mxConstants.STYLE_IMAGE_ALIGN]: mxConstants.ALIGN_CENTER,
+      [mxConstants.STYLE_IMAGE_VERTICAL_ALIGN]: mxConstants.ALIGN_TOP,
+
+      [mxConstants.STYLE_IMAGE_WIDTH]: '72',
+      [mxConstants.STYLE_IMAGE_HEIGHT]: '72',
+      [mxConstants.STYLE_SPACING_TOP]: '105',
+      [mxConstants.STYLE_SPACING]: '8',
+    };
+    this.getStylesheet().putCellStyle('node', nodeStyle);
+
+    // 设置选中状态节点的边角为圆角，默认是直角
+    const oldCreateSelectionShape = mxVertexHandler.prototype.createSelectionShape;
+    mxVertexHandler.prototype.createSelectionShape = function createSelectionShape(...args) {
+      const res = oldCreateSelectionShape.apply(this, args);
+      res.isRounded = true;
+      // style 属性来自 mxShape , mxRectangle 继承自 mxShape
+      res.style = {
+        arcSize: 6,
+      };
+      return res;
+    };
+  }
+
+  _setDefaultEdgeStyle() {
+    // mxGraph.prototype.edgeLabelsMovable = false;
+    // mxGraph.prototype.isCellMovable = cell => !cell.edge;
+
+    const style = this.getStylesheet().getDefaultEdgeStyle();
+    Object.assign(style, {
+      [mxConstants.STYLE_ROUNDED]: true,
+      [mxConstants.STYLE_STROKEWIDTH]: '2',
+      [mxConstants.STYLE_STROKECOLOR]: '#333333',
+      [mxConstants.STYLE_EDGE]: mxConstants.EDGESTYLE_ORTHOGONAL,
+      [mxConstants.STYLE_FONTCOLOR]: '#333333',
+      [mxConstants.STYLE_LABEL_BACKGROUNDCOLOR]: '#ffffff',
+    });
+    // TODO 标注demo 出处
+    // 设置拖拽线的过程出现折线，默认为直线
+    this.connectionHandler.createEdgeState = () => {
+      const edge = this.createEdge();
+      return new mxCellState(this.view, edge, this.getCellStyle(edge));
+    };
+  }
 }
 
 let graph = {};
--- src/pages/AppCanvas.vue
+++ src/pages/AppCanvas.vue
@@ -3,6 +3,26 @@
     class="app-canvas">
     <ElMain
       class="app-canvas__main">
+      <div
+        class="tool-bar">
+        <ElButton
+          type="text"
+          size="mini">
+          导入
+        </ElButton>
+        <ElButton
+          type="text"
+          size="mini"
+          disabled>
+          导出
+        </ElButton>
+        <ElButton
+          type="text"
+          size="mini"
+          disabled>
+          删除
+        </ElButton>
+      </div>
       <div id="graphContainer"/>
     </ElMain>
     <ElAside
@@ -13,16 +33,17 @@
         <div
           id="graphOutline"/>
         <ul
-          class="element-card-list">
+          class="elements">
           <li
+            class="element"
             v-for="(element,idx) in elements"
             :key="idx">
             <img
               v-bind="element"
               class="element-img"
-              :src="`/static/images/${element.icon}`"
-              alt="element">
-            <p>{{ element.name }}</p>
+              :src="`/static/images/ele/${element.icon}`"
+              :alt="element.title">
+            <p>{{ element.title }}</p>
           </li>
         </ul>
       </div>
@@ -35,15 +56,71 @@ import mxgraph from '../graph/index';
 import { genGraph, destroyGraph } from '../graph/Graph';
 
 const {
-  mxOutline
+  mxOutline,
+  mxEvent,
+  mxCell,
+  mxGeometry,
+  mxUtils,
 } = mxgraph;
 
 let graph = null;
 let outline = null;
 
+const makeDraggable = (sourceEles) => {
+  const dropValidate = function (evt) {
+    const x = mxEvent.getClientX(evt);
+    const y = mxEvent.getClientY(evt);
+    // 获取 x,y 所在的元素
+    const elt = document.elementFromPoint(x, y);
+    // 如果鼠标落在graph容器
+    if (mxUtils.isAncestorNode(graph.container, elt)) {
+      return graph;
+    }
+    // 鼠标落在其他地方
+    return null;
+  };
+
+  // drop成功后新建一个节点
+  const dropSuccessCb = function (_graph, evt, target, x, y) {
+    const source = this.element;
+    const src = source.getAttribute('src');
+
+    const nodeRootVertex = new mxCell('Name', new mxGeometry(0, 0, 100, 135), `node;image=${src}`);
+    nodeRootVertex.vertex = true;
+
+    const title = source.getAttribute('alt');
+    const titleVertex = graph.insertVertex(nodeRootVertex, null, title,
+      0.1, 0.65, 80, 16,
+      'constituent=1;whiteSpace=wrap;strokeColor=none;fillColor=none;fontColor=#e6a23c',
+      true);
+    titleVertex.setConnectable(false);
+
+    const normalTypeVertex = graph.insertVertex(nodeRootVertex, null, null,
+      0.05, 0.05, 19, 14,
+      `normalType;constituent=1;fillColor=none;image=/static/images/normal-type/fire.png`,
+      true);
+    normalTypeVertex.setConnectable(false);
+
+    const cells = graph.importCells([nodeRootVertex], x, y, target);
+    if (cells != null && cells.length > 0) {
+      graph.setSelectionCells(cells);
+    }
+  };
+
+  Array.from(sourceEles).forEach((ele) => {
+    const dragElt = document.createElement('img');
+    dragElt.setAttribute('src', ele.getAttribute('src'));
+    dragElt.setAttribute('style', 'width:120px;height:120px;');
+
+    mxUtils.makeDraggable(ele, dropValidate, dropSuccessCb, dragElt,
+      null, null, null, true);
+  });
+};
+
 const initGraph = () => {
   graph = genGraph(document.getElementById('graphContainer'));
   outline = new mxOutline(graph, document.getElementById('graphOutline'));
+  makeDraggable(document.getElementsByClassName('element-img'));
 };
 export default {
   name: 'AppCanvas',
@@ -53,19 +130,19 @@ export default {
       elements: [{
         id: 1,
         icon: 'ele-001.jpeg',
-        name: '比卡丘',
+        title: '比卡丘',
       }, {
         id: 2,
         icon: 'ele-002.png',
-        name: '小火龙',
+        title: '小火龙',
       }, {
         id: 3,
         icon: 'ele-003.jpeg',
-        name: '杰尼龟 故意 把名字 改长点',
+        title: '杰尼龟',
       }, {
         id: 4,
         icon: 'ele-004.jpg',
-        name: '妙蛙种子',
+        title: '妙蛙种子',
       }]
     };
   },
@@ -83,9 +160,15 @@ export default {
       position: relative;
       overflow: hidden;
       width: 100%;
-      height: 96vh;
+      height: 90vh;
       background: url('../assets/images/grid.gif');
       cursor: default;
+    }
+    .tool-bar {
+      background: #eee;
+      padding-left: 10px;
+      border-radius: 4px;
+      margin-bottom: 10px;
     }
   }
   &__right {
@@ -94,16 +177,17 @@ export default {
       border-radius: 4px;
       border: 1px solid #e6e6e6;
     }
-    .element-card-list {
+    .elements {
       margin-top: 20px;
       display: flex;
       flex-wrap: wrap;
       justify-content: space-between;
-      > li {
+      .element {
         width: 100px;
         text-align: center;
         margin-bottom: 10px;
         > img {
+          cursor: pointer;
           width: 100%;
           height: 100px;
         }
